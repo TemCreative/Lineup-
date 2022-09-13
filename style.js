8d7d1efc44e75547b47002e68d796135780828ba
TemCreative <109309894+TemCreative@users.noreply.github.com>
2022-09-13 23:42:39 +0100

    i added
    js

---
+++ style.js
@@ -0,0 +1,33 @@
+class player {
+    constructor(position, jerseyNumber){
+        this.position = position,
+        this.jerseyNumber = jerseyNumber
+       
+    }
+    manager(){
+        console.log("Carlo Ancelotti ")
+    }
+    assistCoach(){
+        console.log("Davide Ancelotti")
+    }
+}
+class Truck extends Car{
+   
+}
+console.log(Car)
+var Courtois = new player  ("Goalkeeper","1")
+console.log(Courtois)
+var Militao = new player  ("Right back","3")
+console.log(Militao)
+var Nacho = new player  ("Left back","6")
+console.log(Nacho)
+var Carvajal = new player  ("Center back","2")
+console.log(Carvajal)
+var Alaba = new player  ("Center back","4")
+console.log(Alaba)
+var Kroos = new player  ("Central midfielder","8")
+console.log(Kroos)
+var Valverde = new player  ("Attacking midfielder","15")
+console.log(Valverde)
+var Camavinga = new player  ("Defensive midfielder","25")
+console.log(Camavinga)
