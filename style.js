9091f9a864718d00d7136e5c72d1d2cc22a35d5f
TemCreative <109309894+TemCreative@users.noreply.github.com>
2022-09-24 20:10:58 +0100

    I added tex-success

--- style.js
+++ style.js
@@ -1,41 +1,67 @@
-class player {
-    constructor(position, jerseyNumber){
-        this.position = position,
-        this.jerseyNumber = jerseyNumber
-       
+class Club {
+    constructor(name, number, natoinality, position) {
+        this.name = name
+        this.number = number
+        this.nationality = natoinality
+        this.position = position
     }
-    manager(){
-        console.log("Carlo Ancelotti ")
-    }
-    assistCoach(){
-        console.log("Davide Ancelotti")
-    }
+    run() { }
 }
+
+var player1 = new Club(" De gea", "1", "Spain", "Gk")
+var player2 = new Club(" Shaw", "23", "England", "LB")
+var player3 = new Club("Lisandro ", "6", "Argentina", "CB")
+var player4 = new Club(" Maguire", "5", "England", "CB")
+var player5 = new Club(" Varane", "19", "France", "RB")
+var player6 = new Club(" Eriksen", "14", "England", "CM")
+var player7 = new Club("Fred", "17", "Brazil", "CM")
+var player8 = new Club("Rashford", "10", "England", "LW")
+var player9 = new Club(" Fernandes", "8", "Portugal", "AM")
+var player10 = new Club("Antony", "21", "Brazil", "RW")
+var player11 = new Club(" Martial", "9", "France", "CF")
+
+console.log(player1)
+
+gk.innerHTML = `<p>${player1.name} <br> <br> ${player1.number}  <br> <br> ${player1.position}  </p> <div id="country" class="mx-auto text-center bg-danger">${player1.nationality}</div> `
+rb.innerHTML = `<p>${player2.name} <br> <br> ${player2.number}  <br> <br> ${player2.position}  </p> <div id="country" class="mx-auto text-center bg-danger">${player2.nationality}</div> `
+cbb.innerHTML = `<p>${player3.name} <br> <br> ${player3.number}  <br> <br> ${player3.position}  </p> <div id="country" class="mx-auto text-center bg-danger">${player3.nationality}</div> `
+rbb.innerHTML = `<p>${player4.name} <br> <br> ${player4.number}  <br> <br> ${player4.position}  </p> <div id="country" class="mx-auto text-center bg-danger">${player4.nationality}</div> `
+cb.innerHTML = `<p>${player5.name} <br> <br> ${player5.number}  <br> <br> ${player5.position}  </p> <div id="country" class="mx-auto text-center bg-danger">${player5.nationality}</div> `
+cmm.innerHTML = `<p>${player6.name} <br> <br> ${player6.number}  <br> <br> ${player6.position}  </p> <div id="country" class="mx-auto text-center bg-danger">${player6.nationality}</div> `
+lw.innerHTML = `<p>${player7.name} <br> <br> ${player7.number}  <br> <br> ${player7.position}  </p> <div id="country" class="mx-auto text-center bg-danger">${player7.nationality}</div> `
+cf.innerHTML = `<p>${player8.name} <br> <br> ${player8.number}  <br> <br> ${player8.position}  </p> <div id="country" class="mx-auto text-center bg-danger">${player8.nationality}</div> `
+cm.innerHTML = `<p>${player9.name} <br> <br> ${player9.number}  <br> <br> ${player9.position}  </p> <div id="country" class="mx-auto text-center bg-danger">${player9.nationality}</div> `
+am.innerHTML = `<p>${player10.name} <br> <br> ${player10.number}  <br> <br> ${player10.position}  </p> <div id="country" class="mx-auto text-center bg-danger">${player10.nationality}</div> `
+rw.innerHTML = `<p>${player11.name} <br> <br> ${player11.number}  <br> <br> ${player11.position}  </p> <div id="country" class="mx-auto text-center bg-danger">${player11.nationality}</div> `
+ 
+
+
+
 // class Truck extends Car{
    
 // }
 // console.log(Car)
-var Courtois = new player  ("Goalkeeper","1")
-console.log(Courtois)
-var Militao = new player  ("Right back","3")
-console.log(Militao)
-var Nacho = new player  ("Left back","6")
-console.log(Nacho)
-var Carvajal = new player  ("Center back","2")
-console.log(Carvajal)
-var Alaba = new player  ("Center back","4")
-console.log(Alaba)
-var Kroos = new player  ("Central back","8")
-console.log(Kroos)
-var Valverde = new player  ("Central midfielder","15")
-console.log(Valverde)
-var Camavinga = new player  ("Attacking midfielder","25")
-console.log(Camavinga)
-var Modric = new player  ("Defensive Midfielder","10")
-console.log(Modric)
-var Asensio = new player  ("Right Wing Forward","11")
-console.log(Asensio)
-var Vinicius = new player  ("Left Wing Forward","20")
-console.log(Vinicius)
-var Benzema = new player  ("Striker","9")
-console.log(Benzema)
+// var Courtois = new player  ("Goalkeeper","1")
+// console.log(Courtois)
+// var Militao = new player  ("Right back","3")
+// console.log(Militao)
+// var Nacho = new player  ("Left back","6")
+// console.log(Nacho)
+// var Carvajal = new player  ("Center back","2")
+// console.log(Carvajal)
+// var Alaba = new player  ("Center back","4")
+// console.log(Alaba)
+// var Kroos = new player  ("Central back","8")
+// console.log(Kroos)
+// var Valverde = new player  ("Central midfielder","15")
+// console.log(Valverde)
+// var Camavinga = new player  ("Attacking midfielder","25")
+// console.log(Camavinga)
+// var Modric = new player  ("Defensive Midfielder","10")
+// console.log(Modric)
+// var Asensio = new player  ("Right Wing Forward","11")
+// console.log(Asensio)
+// var Vinicius = new player  ("Left Wing Forward","20")
+// console.log(Vinicius)
+// var Benzema = new player  ("Striker","9")
+// console.log(Benzema)
